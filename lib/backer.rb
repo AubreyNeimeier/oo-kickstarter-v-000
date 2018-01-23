0f9c77fc85317b6b96416d9ed245e0d0670d3941
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-23 20:24:37 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,8 @@ class Backer
   def back_project(project)
     @backed_projects << project
     project.backers << self
-    #Project.add_backer(self)
+    #the above line. every time a backer adds a project to its backed_projects,
+    #it should also add itself(the backer) to that project's list of bakers (an array set upon initialization of a new project)
   end
 
 end
